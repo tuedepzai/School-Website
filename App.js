086b5dc1b72c9a5666e4f2b8b4285ab4b60159d0
tuedepzai <tuekocopcode@gmail.com>
2025-02-12 01:48:00 +0700

    nganh logistic

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
-console.log("Hello")
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((Entry) =>{
+        console.log(Entry)
+        if(Entry.isIntersecting){
+            Entry.target.classList.add("show");
+        }else{
+            Entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
